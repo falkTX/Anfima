f3e1692568106c882fb9f10c2c6d5a9745ec9e5d
falkTX <falktx@falktx.com>
2026-04-11 00:58:11 +0200

    More robust cmake configure system
    Signed-off-by: falkTX <falktx@falktx.com>

--- utils/res2c.cmake
+++ utils/res2c.cmake
@@ -1,37 +1,40 @@
 # Copyright (C) 2026 Filipe Coelho <falktx@falktx.com>
 # SPDX-License-Identifier: ISC
 
-# cleanup input filename
-string(REPLACE "${CMAKE_CURRENT_BINARY_DIR}/html/" "" INPUT_CLEAN "${INPUT}")
+# set configured filename
+set(CONFIGURED_FULLPATH "${CMAKE_CURRENT_BINARY_DIR}/html/${INPUT_BASEPATH}")
 
 # set output filename
-set(OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/html-data/${INPUT_CLEAN}")
+set(OUTPUT_FULLPATH "${CMAKE_CURRENT_BINARY_DIR}/html-data/${INPUT_BASEPATH}")
 
-# get file length
-file(SIZE "${INPUT}" INPUT_CONTENT_LEN)
+# run cmake configure_file
+configure_file("${INPUT_FULLPATH}" "${CONFIGURED_FULLPATH}" @ONLY)
 
-# read input file
-file(READ "${INPUT}" INPUT_CONTENT_HEX HEX)
+# get configured file length
+file(SIZE "${CONFIGURED_FULLPATH}" CONFIGURED_CONTENT_LEN)
 
-# convert input file content into C hexadecimal array
-string(REGEX REPLACE "([0-9a-f][0-9a-f])" "0x\\1," INPUT_CONTENT_HEX "${INPUT_CONTENT_HEX}")
+# read configured file
+file(READ "${CONFIGURED_FULLPATH}" CONFIGURED_CONTENT_HEX HEX)
 
-# generate C-compatbile symbol based on input name
-string(MAKE_C_IDENTIFIER "${INPUT_CLEAN}" INPUT_SYMBOL)
+# convert configured file content into C hexadecimal array
+string(REGEX REPLACE "([0-9a-f][0-9a-f])" "0x\\1," CONFIGURED_CONTENT_HEX "${CONFIGURED_CONTENT_HEX}")
+
+# generate C-compatible symbol based on input name
+string(MAKE_C_IDENTIFIER "${INPUT_BASEPATH}" INPUT_SYMBOL)
 string(TOUPPER "${INPUT_SYMBOL}" INPUT_SYMBOL)
 
 # generate C++ files
 set(OUT_CPP "// auto-generated file
-#include \"html-data/${INPUT_CLEAN}.h\"
-const unsigned char ${INPUT_SYMBOL}_DATA[] = { ${INPUT_CONTENT_HEX} };
+#include \"html-data/${INPUT_BASEPATH}.h\"
+const unsigned char ${INPUT_SYMBOL}_DATA[] = { ${CONFIGURED_CONTENT_HEX} };
 static_assert(sizeof(${INPUT_SYMBOL}_DATA) == ${INPUT_SYMBOL}_LEN, \"Incorrect auto-generated size\");
 ")
 
 set(OUT_H "// auto-generated file
 #pragma once
-#define ${INPUT_SYMBOL}_LEN ${INPUT_CONTENT_LEN}
+#define ${INPUT_SYMBOL}_LEN ${CONFIGURED_CONTENT_LEN}
 extern const unsigned char ${INPUT_SYMBOL}_DATA[];
 ")
 
-file(WRITE "${OUTPUT}.cpp" "${OUT_CPP}")
-file(WRITE "${OUTPUT}.h" "${OUT_H}")
+file(WRITE "${OUTPUT_FULLPATH}.cpp" "${OUT_CPP}")
+file(WRITE "${OUTPUT_FULLPATH}.h" "${OUT_H}")
